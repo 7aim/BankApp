2d8d38c9d881836bfa92e2e55ab9707be8f379de
Aim <7asim6@gmail.com>
2025-03-29 00:52:21 +0400

    user dict

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
-#login
-id = 123
-name = input("Name : ")
-password = input("Password : ")
-card_no = input("Card Number : ")
+import random
+users = {}
+while True:
+    chooice = input("1/2 : ")
+    if chooice == "1":
+        #login
+        id = 1
+        name = input("Name : ")
+        password = input("Password : ")
+        card_no = input("Card Number : ")
+        balance = 0
+        log = []
 
+        users[name] = {"id" : id,'password': password,"card_no" : card_no,"balance" : balance,"log" : log}
+        id += 1
+        print(users)
 
-user = {"id" : id,
-        "name" : name,
-        "password" : password,
-        "card_no" : card_no
-        }
+    if chooice == "2":
+        name = input("Name : ")
+        for i in users:     
+            while name not in users:
+                print("False")
+                name = input("Name : ")
 
-#singin
-name = input("Name : ")
-if name in user["name"]:
-    print("1")
-else:
-    print("2")
+        password = input("Password : ")
+        while password not in users[name]["password"]:
+            print("False")
+            password = input("Password : ")
 
-password = input("Password : ")
-if password in user["password"]:
-    print("1")
-else:
-    print("2")
-
-card_no = input("Card Number : ")
-if card_no in user["card_no"]:
-    print("1")
-else:
-    print("2")
+        card_no = input("Card Number : ")
+        while card_no not in users[name]["card_no"]:
+            print("False")
+            card_no = input("Card Number : ")
